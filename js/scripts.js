b636b65ab2f6ee1022a9aca590cd99da304052f8
Marta <maartamulet@gmail.com>
2025-05-07 17:49:13 +0200

    error nav-toggle

--- js/scripts.js
+++ js/scripts.js
@@ -10,7 +10,7 @@ backToTopButton.addEventListener("click", () => {
 });
 
 
-const toggle = document.getElementById('nav-toggle');
+const toggle = document.getElementById('.nav-toggle');
 const menu = document.getElementById('nav-menu');
 
 toggle.addEventListener('click', () => {
@@ -56,14 +56,3 @@ window.addEventListener("scroll", function () {
   lastScrollTop = scrollTop;
 });
 
-
-/*
-const pagina = document.querySelector(".pagina");
-const toggleButton = document.getElementById("darkModeToggle");
-
-toggleButton.classList.add("mode-toggle");
-
-toggleButton.addEventListener("click", () => {
-  pagina.classList.toggle("fosc");
-});
-*/
