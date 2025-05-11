041f67411fd196357d830d780ca96d484bf582a7
Marta <maartamulet@gmail.com>
2025-05-11 19:50:59 +0200

    borrar mapa antic

--- maps/peluts/data/protectores_animals__4.js
+++
@@ -1,1 +0,0 @@
-var json_protectores_animals__4 = {"type":"FeatureCollection","name":"protectores_animals__4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
